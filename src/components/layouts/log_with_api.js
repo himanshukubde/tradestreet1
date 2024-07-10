31f439358c6f53587f9bb5a9222596913c753881
snehpnp <snehpnp@gmail.com>
2024-07-10 16:26:21 +0530

    Done

--- src/components/layouts/log_with_api.js
+++ src/components/layouts/log_with_api.js
@@ -4,21 +4,72 @@ import * as Config from "../../Utils/Config";
 
 const loginWithApi = async (UserDetails) => {
     console.log("Broker - ", UserDetails.BrokerName);
-    console.log("Base Url - ",Config.base_url);
+    console.log("Base Url - ", Config.base_url);
 
 
     if (UserDetails) {
 
-        // if (UserDetails.BrokerName.toUpperCase() === "ANGEL") {
-        //     console.log("ANGEL", UserDetails);
-        // }
+        if (UserDetails.BrokerName.toUpperCase() === "Aliceblue".toUpperCase()) {
+            console.log("ALICE", UserDetails);
+
+            var data = {
+                Username: UserDetails.Username,
+                session: "",
+                AccToken: "",
+                usrid: "",
+                sid: "",
+                jwt_Token: "",
+            }
+
+            try {
+                const response = await axios.post(`${Config.base_url}ConnectBroker`, data);
+
+                if (response.data.Status) { // Assuming the status is in response.data.Status
+
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Trading On successfully.',
+                        icon: 'success',
+                        confirmButtonText: 'OK',
+                        timer: 1000
+                    }).then(() => {
+                        setTimeout(() => {
+                            window.location.reload();
+                        }, 1000);
+                    });
+                } else {
+
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'Trading Off successfully.',
+                        icon: 'error',
+                        confirmButtonText: 'OK',
+                        timer: 1000
+                    }).then(() => {
+                        setTimeout(() => {
+                            window.location.reload();
+
+                        }, 1000);
+                    });
+                }
+            } catch (err) {
+                console.error("Error in ConnectBroker request", err);
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'An error occurred. Please try again later.',
+                    icon: 'error',
+                    confirmButtonText: 'OK',
+                });
+            }
+        }
+
 
         if (UserDetails.BrokerName.toUpperCase() === "ANGEL") {
-      
+
             let config = {
                 method: 'get',
                 maxBodyLength: Infinity,
-                url: 'http://193.239.237.95:8000/AngelBroker/'+UserDetails.Username,
+                url: 'http://193.239.237.95:8000/AngelBroker/' + UserDetails.Username,
                 headers: {}
             };
 
@@ -40,17 +91,17 @@ const loginWithApi = async (UserDetails) => {
         if (UserDetails.BrokerName.toUpperCase() === "ICICI") {
             console.log("ICICI", UserDetails);
 
-            
+
             let config = {
                 method: 'get',
                 maxBodyLength: Infinity,
-                url: Config.base_url+'ICICIBroker/'+UserDetails.Username,
+                url: Config.base_url + 'ICICIBroker/' + UserDetails.Username,
                 headers: {}
             };
 
             axios.request(config)
                 .then((response) => {
-                    console.log("response",response)
+                    console.log("response", response)
                     if (response.data.Status == true) {
 
                         window.location.href = response.data.Api;
@@ -65,17 +116,17 @@ const loginWithApi = async (UserDetails) => {
         if (UserDetails.BrokerName.toUpperCase() === "UPSTOX") {
             console.log("UpstoxBroker", UserDetails);
 
-            
+
             let config = {
                 method: 'get',
                 maxBodyLength: Infinity,
-                url: Config.base_url+'UpstoxBroker/'+UserDetails.Username,
+                url: Config.base_url + 'UpstoxBroker/' + UserDetails.Username,
                 headers: {}
             };
 
             axios.request(config)
                 .then((response) => {
-                    console.log("response",response)
+                    console.log("response", response)
                     if (response.data.Status == true) {
 
                         window.location.href = response.data.Api;
@@ -90,17 +141,42 @@ const loginWithApi = async (UserDetails) => {
         if (UserDetails.BrokerName.toUpperCase() === "5PAISA") {
             console.log("5PAISA", UserDetails);
 
-            
+
             let config = {
                 method: 'get',
                 maxBodyLength: Infinity,
-                url: Config.base_url+'5PaisaBroker/'+UserDetails.Username,
+                url: Config.base_url + '5PaisaBroker/' + UserDetails.Username,
                 headers: {}
             };
 
             axios.request(config)
                 .then((response) => {
-                    console.log("response",response)
+                    console.log("response", response)
+                    if (response.data.Status == true) {
+
+                        window.location.href = response.data.Api;
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+
+
+        if (UserDetails.BrokerName.toUpperCase() === "MASTERTRUST") {
+            console.log("MASTERTRUST", UserDetails);
+
+
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: Config.base_url + 'MastertrustBroker/' + UserDetails.Username,
+                headers: {}
+            };
+
+            axios.request(config)
+                .then((response) => {
+                    console.log("response", response)
                     if (response.data.Status == true) {
 
                         window.location.href = response.data.Api;
