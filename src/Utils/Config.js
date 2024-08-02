2f3427b79ae47b820bee83cec0c80013a334c0f5
Chandra <cppatel6388@gmail.com>
2024-08-02 16:23:08 +0530

    done

--- src/Utils/Config.js
+++ src/Utils/Config.js
@@ -1,11 +1,11 @@
 export const react_domain = `${window.location.origin}`;
 // export const base_url = `http://193.239.237.95:8002/`;
 // export const base_url = `http://193.239.237.147:8000/`;
-// export const base_url = `http://193.239.237.150:8000/`;
+
+
+
+
+export const base_url = `http://193.239.237.149:8000/`;
 // export const base_url = `https://tools.tradestreet.in/backend/`;
-
-
-
-
-export const base_url = `https://tools.nextbrand.tech/backend/`;
+// export const base_url = `https://tools.nextbrand.tech/backend/`;
  
