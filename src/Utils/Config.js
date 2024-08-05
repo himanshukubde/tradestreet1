c91b95ea7c795204b23048e5237267b208c81271
Chandra <cppatel6388@gmail.com>
2024-08-05 19:04:03 +0530

    gfg

--- src/Utils/Config.js
+++ src/Utils/Config.js
@@ -1,6 +1,6 @@
 export const react_domain = `${window.location.origin}`;
 
-// export const base_url = `${window.location.origin}/backend/`
+export const base_url = `${window.location.origin}/backend/`
 // export const base_url = `http://193.239.237.95:8002/`;
 // export const base_url = `http://193.239.237.147:8000/`;
 
@@ -13,6 +13,6 @@ export const react_domain = `${window.location.origin}`;
 
 
 
-export const base_url = `https://tools.tradestreet.in/backend/`;
+// export const base_url = `https://tools.tradestreet.in/backend/`;
 // export const base_url = `https://tools.nextbrand.tech/backend/`;
  
