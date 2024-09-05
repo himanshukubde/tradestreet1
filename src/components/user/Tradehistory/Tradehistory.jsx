a0dec35f4c232a876b3837c65806e7ab0ed86182
Chandra <cppatel6388@gmail.com>
2024-09-05 11:42:16 +0530

    done

--- src/components/user/Tradehistory/Tradehistory.jsx
+++ src/components/user/Tradehistory/Tradehistory.jsx
@@ -519,7 +519,7 @@ const Tradehistory = () => {
                                         <div class="accordion-item">
                                             <h2 class="accordion-header" id="headingTwo">
                                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={{fontWeight: 'bold'}}>
-                                                    Drawdown Table cpp
+                                                    Drawdown Table
                                                 </button>
 
                                             </h2>
